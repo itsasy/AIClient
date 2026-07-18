288d25a55da73da4109a2f761b2bc649aa92ffac
Alexis <alex.maldonado@cxhelios.com>
2026-07-17 21:29:17 -0300

    Fix simple rag error

--- core/context_builder.py
+++ core/context_builder.py
@@ -1,9 +1,9 @@
 from core.project_inspector import ProjectInspector
-from obsidian.rag import SimpleRAG
+from obsidian.rag import RAG
 
 class ContextBuilder:
     def __init__(self):
-        self.rag = SimpleRAG()
+        self.rag = RAG()
         self.inspector = ProjectInspector()
 
     def build(self, query: str) -> dict:
